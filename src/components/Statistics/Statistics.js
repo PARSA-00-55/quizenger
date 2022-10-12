7a076913a9f58c96905adf0c4174b20775c6e56d
PARSA-00-55 <parsa02345@gmail.com>
2022-10-12 16:44:54 +0600

    statistics added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,12 +1,63 @@
-import React from 'react';
-import './Statistics.css';
+import React from "react";
+import "./Statistics.css";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const Statistics = () => {
-    return (
-        <div>
-            <h4>This is statistics page</h4>
-        </div>
-    );
+
+
+  const data = [
+    {
+      name: "React",
+      Question: 8,
+    },
+    {
+      name: "JavaScript",
+      Question: 9,
+    },
+    {
+      name: "CSS",
+      Question: 8,
+    },
+    {
+      name: "Git",
+      Question: 11,
+    },
+    
+  ];
+
+  return (
+    <div className="container mt-5">
+      <div className="chart">
+      <BarChart
+          width={350}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="Question" fill="#82ca9d" />
+        </BarChart>
+      </div>
+    </div>
+  );
 };
 
 export default Statistics;
